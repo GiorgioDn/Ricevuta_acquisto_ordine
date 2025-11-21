9f859d79cfdd00c511f76421590f226397d1881b
Antonia Guerra <a.guerra12@studenti.unimol.it>
2025-11-21 16:29:53 +0100

    utente e salvadanaio
    esercizio

---
+++ Antonia/utente.py
@@ -0,0 +1,35 @@
+#crea una classe utente con attributi nome, cognome, età e email
+
+class Utente:
+    def __init__(self, nome: str, cognome: str, eta: int, email: str, recapito: str, salvadanaio):
+        self.nome = nome
+        self.cognome = cognome
+        self.eta = eta
+        self.email = email
+        self.recapito = recapito
+        self.salvadanaio = salvadanaio
+        
+    def richiesta_acquisto(self, costo):
+        #verifica se l'utente ha budget sufficiente per effettuare un acquisto
+        return self.salvadanaio.ha_budget(costo)
+        #controlla se l'acquisto può essere effettuato e stampa il risultato
+        if self.salvadanaio.spendi(costo):
+            print("Acquisto riuscito!")
+        else:
+            print("Fondi insufficienti! Acquisto negato.")
+    
+#classe salvadanaio con attributo budget e metodo per controllare se il budget è sufficiente per un costo dato
+class Salvadanaio:
+    def __init__(self, budget: float):
+        self.budget = budget
+        
+    def ha_budget(self, costo):
+        #controlla se il costo è coperto dal budget disponibile
+        return self.budget >= costo
+    
+    def spendi(self, costo):
+        if self.ha_budget(costo):
+            # deduce il costo dal budget per simulare una spesa
+            self.budget -= costo
+            return True
+        return False
